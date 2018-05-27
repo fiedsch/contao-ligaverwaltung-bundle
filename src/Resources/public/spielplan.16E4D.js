86d08b33f1fdef9990712cb4c36a3b042c632bce
Andreas Fieger <fiedsch@ja-eh.at>
2018-05-27 11:29:43 +0200

    update
    dem Stand des gedruckten Spielplans anpassen

--- src/Resources/public/spielplan.16E4D.js
+++ src/Resources/public/spielplan.16E4D.js
@@ -25,8 +25,8 @@
  { home: [1], away: [3] },
  // - - - - - - - - - - -
  // Doppel
- { home: [0,1], away: [1,0] },
- { home: [2,3], away: [3,2] },
- { home: [2,3], away: [0,1] },
- { home: [0,1], away: [2,3] }
+ { home: [0,2], away: [1,3] },
+ { home: [1,3], away: [0,2] },
+ { home: [0,2], away: [0,2] },
+ { home: [1,3], away: [1,3] }
  ];
