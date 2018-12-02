dc0ab8131abcb375bcc237ac2616b8681add1a39
Andreas Fieger <fiedsch@ja-eh.at>
2018-12-02 11:58:02 +0100

    group use statements

--- src/Command/RechnungsDatenAbzugCommand.php
+++ src/Command/RechnungsDatenAbzugCommand.php
@@ -10,12 +10,12 @@
 
 namespace Fiedsch\LigaverwaltungBundle\Command;
 
-use Contao\SaisonModel;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Contao\LigaModel;
+use Contao\SaisonModel;
 use Contao\MannschaftModel;
 
 /**
