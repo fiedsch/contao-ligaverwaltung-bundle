edc7fa15dd2a43ba5615a54c7511362a8ff3f432
Andreas Fieger <fiedsch@ja-eh.at>
2018-12-12 08:43:49 +0100

    do not leave template variables empty
    which we would if we return early

--- src/Resources/contao/elements/ContentSpielerliste.php
+++ src/Resources/contao/elements/ContentSpielerliste.php
@@ -42,8 +42,8 @@ class ContentSpielerliste extends ContentElement
                 'order' => 'teamcaptain DESC, co_teamcaptain DESC, firstname ASC, lastname ASC',
             ]);
 
-            return;
         if (!$allespieler) {
+            $allespieler = [];
         }
 
         $listitems = [];
