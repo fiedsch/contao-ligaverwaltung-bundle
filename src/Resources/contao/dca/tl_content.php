2aa74d21ce48b3ed649a71e79e6570df3bf1bba5
Andreas Fieger <fiedsch@ja-eh.at>
2019-09-29 15:28:01 +0200

    add custom template

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -133,7 +133,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['rankingfield'] = [
 ];
 
 /* Mannschaftenübersicht: Mannschaft und deren Teamcaptains */
-$GLOBALS['TL_DCA']['tl_content']['palettes']['mannschaftenuebersicht'] = '{type_legend},type,headline;{saison_legend},saison;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['mannschaftenuebersicht'] = '{type_legend},type,headline;{saison_legend},saison;{protected_legend:hide},protected;{template_legend},customTpl;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['ligen'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['ligen'],
