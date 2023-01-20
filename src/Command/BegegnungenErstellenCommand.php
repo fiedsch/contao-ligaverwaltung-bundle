4a92f92380382463cd4ce0072f2743c76a681ca2
Andreas Fieger <fiedsch@ja-eh.at>
2023-01-20 14:29:02 +0100

    Update copyright notice

--- src/Command/BegegnungenErstellenCommand.php
+++ src/Command/BegegnungenErstellenCommand.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of fiedsch/ligaverwaltung-bundle.
  *
- * (c) 2016-2021 Andreas Fieger
+ * (c) 2016-2023 Andreas Fieger
  *
  * @package Ligaverwaltung
  * @link https://github.com/fiedsch/contao-ligaverwaltung-bundle/
--- src/Element/ContentTeamsAndPlayersOverview.php
+++ src/Element/ContentTeamsAndPlayersOverview.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of fiedsch/ligaverwaltung-bundle.
+ *
+ * (c) 2016-2023 Andreas Fieger
+ *
+ * @package Ligaverwaltung
+ * @link https://github.com/fiedsch/contao-ligaverwaltung-bundle/
+ * @license https://opensource.org/licenses/MIT
+ */
+
 namespace Fiedsch\LigaverwaltungBundle\Element;
 
 use Contao\BackendTemplate;
