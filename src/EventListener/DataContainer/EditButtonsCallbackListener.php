e5e44b02bed315ac58b6b6bf88333b8e252b3597
Andreas Fieger <fiedsch@ja-eh.at>
2026-01-26 10:41:44 +0100

    Remove some of the buttons in tl_begegnung (saveNduplicate and saveNcreate) as the dont make sense there

---
+++ src/EventListener/DataContainer/EditButtonsCallbackListener.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fiedsch\LigaverwaltungBundle\EventListener\DataContainer;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\DataContainer;
+
+#[AsCallback(table: 'tl_begegnung', target: 'edit.buttons')]
+
+class EditButtonsCallbackListener
+{
+    public function __invoke(array $buttons, DataContainer $dc): array
+    {
+        // Remove buttons that don't make sense here
+        // unset($buttons['saveNclose']);
+        unset($buttons['saveNcreate']);
+        unset($buttons['saveNduplicate']);
+
+        return $buttons;
+    }
+}
