256349635f72070434f99cf3ab20d569b2af6bc0
Andreas Fieger <fiedsch@ja-eh.at>
2026-02-07 10:21:58 +0100

    Minor fix of inline JS

--- src/Resources/contao/dca/tl_begegnung.php
+++ src/Resources/contao/dca/tl_begegnung.php
@@ -141,7 +141,7 @@ $GLOBALS['TL_DCA']['tl_begegnung'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_begegnung']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'));return false;Backend.getScrollOffset()"',
             ],
             'toggle' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_begegnung']['toggle'],
