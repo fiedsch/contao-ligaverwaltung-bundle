6e7be7e5e60c94177e8c451eca52bd2364c0d72f
Andreas Fieger <fiedsch@ja-eh.at>
2026-02-14 11:38:02 +0100

    Remove verband from palette (like everywhere else)

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -24,7 +24,7 @@ use Fiedsch\Ligaverwaltung\Helper\DCAHelper;
 use Fiedsch\Ligaverwaltung\Model\HighlightModel;
 
 /* Ligenliste */
-$GLOBALS['TL_DCA']['tl_content']['palettes']['ligenliste'] = '{type_legend},type,headline;{auswahl_legend},verband,saison;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['ligenliste'] = '{type_legend},type,headline;{auswahl_legend},saison;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['fields']['verband'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['verband'],
     'exclude' => true,
